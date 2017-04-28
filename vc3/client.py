82c3cbf0eaf76752b35e3edff6f8b4cd615bbc83
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-04-28 12:50:16 -0400

    Update.

--- vc3/client.py
+++ vc3/client.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 __author__ = "John Hover"
 __copyright__ = "2017 John Hover"
 __credits__ = []
@@ -7,11 +8,11 @@ __maintainer__ = "John Hover"
 __email__ = "jhover@bnl.gov"
 __status__ = "Production"
 
-#
-# Abstract client library
-#
-
+import argparse
 import logging
+import os
+
+from ConfigParser import ConfigParser
 
 from entities import User, Project, Resource, Allocation, Request, Cluster, Application
 from vc3 import infoclient
@@ -22,20 +23,44 @@ class VC3ClientAPI(object):
         self.ic = infoclient.InfoClient(self.config)
         self.log = logging.getLogger() 
 
-    def createUser(self,   
+
+    def defineUser(self,   
                    name,
                    first,
                    last,
                    email,
                    institution):           
         '''
-         
+       Defines a new User object for usage elsewhere in the API. 
+              
+       :param str name: The unique VC3 username of this user
+       :param str first: User's first name
+       :param str last: User's last name
+       :param str email: User's email address
+       :param str institution: User's intitutional affiliation or employer
+       :return: User  A valid User object
+       
+       :rtype: User        
         '''
         u = User( name, first, last, email, institution)
         self.log.debug("Creating user: %s " % u)
+        return u
+    
         
+    def createUser(self, user, infoclient=None):
+        '''
+        Stores the provided user in the infoservice. 
         
-    def updateUser(self, name, ):
+        :param User u:  User to add. 
+        :return: None
+        '''
+        if infoclient is None:
+            infoclient = self.ic
+        user.store(infoclient)
+        
+                
+        
+    def updateUser(self, u ):
         '''
         
         '''
@@ -45,15 +70,20 @@ class VC3ClientAPI(object):
         
 
         print(out)
-       
-    def updateUser(self):
+
+    def getUser(self, username):
         pass
+
     
     def createProject(self):
         pass
     
     def updateProject(self):
         pass
+    
+    def showProject(self):
+        pass
+    
         
     def createResource(self):
         pass
@@ -79,5 +109,102 @@ class EntityExistsException(Exception):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+
+class VC3ClientCLI(object):
+    '''
     
+    '''
+    def __init__(self):
+        self.parseopts()
+        self.setuplogging()
+
+    def parseopts(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-c', '--config', 
+                            action="store", 
+                            dest='configpath', 
+                            default='~/etc/vc3client.conf', 
+                            help='configuration file path.')
+        
+        parser.add_argument('-d', '--debug', 
+                            action="store_true", 
+                            dest='debug', 
+                            help='debug logging')        
+        
+        # Init sub-command
+        subparsers = parser.add_subparsers( dest="subcommand")
+
+        parser_usercreate = subparsers.add_parser('user-create', 
+                                                help='create new vc3 user')
+        parser_usercreate.add_argument('username', 
+                                     action="store")
+        
+        parser_usercreate.add_argument('--firstname', 
+                                     action="store", 
+                                     dest="firstname", 
+                                     default='unknown')
+
+        parser_usercreate.add_argument('--lastname', 
+                                     action="store", 
+                                     dest="lastname", 
+                                     default='unknown')
+
+        parser_usercreate.add_argument('--email', 
+                                     action="store", 
+                                     dest="email", 
+                                     default='unknown')
+
+        parser_usercreate.add_argument('--institution', 
+                                     action="store", 
+                                     dest="institution", 
+                                     default='unknown')     
+
+        self.results= parser.parse_args()
+
+
+
+    def setuplogging(self):
+        self.log = logging.getLogger()
+        FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
+        formatter = logging.Formatter(FORMAT)
+        #formatter.converter = time.gmtime  # to convert timestamps to UTC
+        logStream = logging.StreamHandler()
+        logStream.setFormatter(formatter)
+        self.log.addHandler(logStream)
+    
+        self.log.setLevel(logging.WARN)
+        if self.results.debug:
+            self.log.setLevel(logging.DEBUG)
+        self.log.info('Logging initialized.')
+
+
+    def invoke(self):
+        cp = ConfigParser()
+        ns = self.results
+        self.log.info("Config is %s" % ns.configpath)
+        cp.read(os.path.expanduser(ns.configpath))
+
+        capi = VC3ClientAPI(cp)
+        
+        if ns.subcommand == 'user-create':
+            
+            u = capi.defineUser( ns.username,
+                             ns.firstname,
+                             ns.lastname,
+                             ns.email,
+                             ns.institution)
+            self.log.debug("User is %s" % u)
+            capi.createUser(u)
+            
+        else:
+            self.log.warning('Unrecognized subcommand is %s' % ns.subcommand)
+            
+
+if __name__ == '__main__':
+    vc3cli = VC3ClientCLI()
+    vc3cli.invoke()    
+
+
+
 
--- vc3/entities.py
+++ vc3/entities.py
@@ -1,8 +1,38 @@
+#!/bin/env python
 
+__author__ = "John Hover"
+__copyright__ = "2017 John Hover"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.9.1"
+__maintainer__ = "John Hover"
+__email__ = "jhover@bnl.gov"
+__status__ = "Production"
+
+'''
+
+    + User
+    Users always get a default Project, containing them as owner and member. 
+    Users may add other users to projects they own.   
+    User creates one or more Projects
+    
+    Projects contain users as members.
+    
+    + Resources     
+       Admin users can define new Resources
+       
+    + Allocation
+        Users can add their own Allocations on a given Resource. 
+
+
+'''
+
+import logging
 
 class User(object):
     '''
-    Represents a VC3 user account. 
+    Represents a VC3 user account.
+    As policy, name, email, and institution must be set.  
 {
     "user" : {
         "johnrhover": {
@@ -13,8 +43,6 @@ class User(object):
         },
 }
 
-
-
     '''
     def __init__(self, 
                    name,
@@ -22,11 +50,26 @@ class User(object):
                    last,
                    email,
                    institution):
+        '''
+        Defines a new User object for usage elsewhere in the API. 
+              
+        :param str name: The unique VC3 username of this user
+        :param str first: User's first name
+        :param str last: User's last name
+        :param str email: User's email address
+        :param str institution: User's intitutional affiliation or employer
+        :return: User  A valid User object
+       
+        :rtype: User
+        '''  
+        self.log = logging.getLogger()
         self.name = name
         self.first = first
         self.last = last
         self.email = email
         self.institution = institution
+        self.doc_attributes = ["first",'last','email','institution']
+        self.log.debug("User object created: %s" % self)
         
     def __repr__(self):
         s =  "User(name=%s, first=%s, last=%s, email=%s, institution=%s " % (self.name,
@@ -35,7 +78,30 @@ class User(object):
             self.email,
             self.institution)
         return s
-    
+
+    def makeDictObject(self):
+        '''
+        Converts Python object to attribute dictionary suitable for addition to existing dict 
+        intended to be converted back to JSON. Uses <obj>.name as key:
+        
+        '''
+        d = {}
+        d[self.name] = {}
+        for attrname in self.doc_attributes:
+            d[self.name][attrname] = getattr(self, attrname)
+        self.log.debug("Returning dict: %s" % d)
+        return d
+        
+        
+    def store(self, infoclient):
+        '''
+        Stores this user in the provided infoclient info tree. 
+        '''
+        users = infoclient.getdocumentobject(key='user')
+        du = self.makeDictObject()
+        self.log.debug("Dict obj: %s" % du)
+        infoclient.storedocumentobject(du, key='user')
+                
         
           
 
@@ -98,5 +164,21 @@ class Application(object):
     '''
 
 
+def runtest():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    log = logging.getLogger()
+    u = User(name='vc3jhover', 
+             first='John', 
+             last='Hover', 
+             email='jhover@bnl.gov', 
+             institution = 'BNL')
+    log.info("User made %s" % u)
+    du = u.makeDictObject()
+    log.info("Dict made %s" % du)
+    #u.store()
+
+
+if __name__ == '__main__':
+    runtest()
     
 
