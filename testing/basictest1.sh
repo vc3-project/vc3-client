1143e446c7623602fe46acda83d74b520d4df4bd
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-06-19 15:41:39 -0400

    Update.

--- testing/basictest1.sh
+++ testing/basictest1.sh
@@ -3,40 +3,40 @@ DEBUG='-d'
 CLIENT=~/git/vc3-client/vc3client/clientcli.py
 CONFIG=~/git/vc3-client/etc/vc3-client.conf
 
-echo $CLIENT -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL jhover
-$CLIENT -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL jhover
-echo $CLIENT -c $CONFIG user-create --firstname Angus --lastname MacGuyver --email angus@bnl.gov --institution BNL angus
-$CLIENT -c $CONFIG user-create --firstname Angus --lastname MacGuyver --email angus@bnl.gov --institution BNL angus
-echo $CLIENT -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL adminjhover
-$CLIENT -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL adminjhover
+echo $CLIENT $DEBUG -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL jhover
+$CLIENT $DEBUG -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL jhover
+echo $CLIENT $DEBUG -c $CONFIG user-create --firstname Angus --lastname MacGuyver --email angus@bnl.gov --institution BNL angus
+$CLIENT $DEBUG -c $CONFIG user-create --firstname Angus --lastname MacGuyver --email angus@bnl.gov --institution BNL angus
+echo $CLIENT $DEBUG -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL adminjhover
+$CLIENT $DEBUG -c $CONFIG user-create --firstname John --lastname Hover --email jhover@bnl.gov --institution BNL adminjhover
 
-echo $CLIENT -c $CONFIG user-list
-$CLIENT -c $CONFIG user-list
+echo $CLIENT $DEBUG -c $CONFIG user-list
+$CLIENT $DEBUG -c $CONFIG user-list
 
-echo $CLIENT -c $CONFIG project-create --owner jhover --members jhover  jhoverproject
-$CLIENT -c $CONFIG project-create --owner jhover --members jhover  jhoverproject
-echo $CLIENT -c $CONFIG project-adduser jhoverproject angus
-$CLIENT -c $CONFIG project-adduser jhoverproject angus
+echo $CLIENT $DEBUG -c $CONFIG project-create --owner jhover --members jhover  jhoverproject
+$CLIENT $DEBUG -c $CONFIG project-create --owner jhover --members jhover  jhoverproject
+echo $CLIENT $DEBUG -c $CONFIG project-adduser jhoverproject angus
+$CLIENT $DEBUG -c $CONFIG project-adduser jhoverproject angus
 
-echo $CLIENT -c $CONFIG project-list
-$CLIENT -c $CONFIG project-list
+echo $CLIENT $DEBUG -c $CONFIG project-list
+$CLIENT $DEBUG -c $CONFIG project-list
 
-echo $CLIENT -c $CONFIG project-list --project jhoverproject
-$CLIENT -c $CONFIG project-list --project jhoverproject
+echo $CLIENT $DEBUG -c $CONFIG project-list --project jhoverproject
+$CLIENT $DEBUG -c $CONFIG project-list --project jhoverproject
 
-echo $CLIENT -c $CONFIG resource-create  --owner adminjhover --accesstype remote-batch --accessmethod ssh --accessflavor slurm   sdcc-ic
-$CLIENT -c $CONFIG resource-create  --owner adminjhover --accesstype remote-batch --accessmethod ssh --accessflavor slurm sdcc-ic
+echo $CLIENT $DEBUG -c $CONFIG resource-create  --owner adminjhover --accesstype remote-batch --accessmethod ssh --accessflavor slurm   sdcc-ic
+$CLIENT $DEBUG -c $CONFIG resource-create  --owner adminjhover --accesstype remote-batch --accessmethod ssh --accessflavor slurm sdcc-ic
 
-echo $CLIENT -c $CONFIG resource-list
-$CLIENT -c $CONFIG resource-list
+echo $CLIENT $DEBUG -c $CONFIG resource-list
+$CLIENT $DEBUG -c $CONFIG resource-list
 
-echo $CLIENT -c $CONFIG resource-list --resource sdcc-ic
-$CLIENT -c $CONFIG resource-list --resource sdcc-ic
+echo $CLIENT $DEBUG -c $CONFIG resource-list --resource sdcc-ic
+$CLIENT $DEBUG -c $CONFIG resource-list --resource sdcc-ic
 
-echo $CLIENT -c $CONFIG allocation-create --owner jhover --resource sdcc-ic --accountname jhover  jhover.sdcc-ic
-$CLIENT -c $CONFIG allocation-create --owner jhover --resource sdcc-ic --accountname jhover jhover.sdcc-ic
+echo $CLIENT $DEBUG -c $CONFIG allocation-create --owner jhover --resource sdcc-ic --accountname jhover  jhover.sdcc-ic
+$CLIENT $DEBUG -c $CONFIG allocation-create --owner jhover --resource sdcc-ic --accountname jhover jhover.sdcc-ic
 
-echo $CLIENT -c $CONFIG allocation-list
-$CLIENT -c $CONFIG allocation-list
+echo $CLIENT $DEBUG -c $CONFIG allocation-list
+$CLIENT $DEBUG -c $CONFIG allocation-list
 
 
