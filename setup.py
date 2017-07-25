dc41cc0e992a383055203aed85a0896ee8db169c
Suchandra Thapa <sthapa@ci.uchicago.edu>
2017-07-25 12:45:39 -0500

    Clean up and commit version that works with pip and user installs

--- setup.py
+++ setup.py
@@ -4,51 +4,38 @@
 #
 #
 
-release_version='0.9.1'
-
 import sys
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-etc_files = ['etc/vc3-client.conf']
+release_version='0.9.1'
+
+etc_files = ['./etc/vc3-client.conf']
 
 scripts = ['scripts/vc3-client', ]
 
-rpm_data_files = [('/usr/bin', scripts),
-                ('/etc/vc3', etc_files),
-               ]
 
-home_data_files = [
-                 #('etc', libexec_files),
-                 ('etc', etc_files),
-                ]
+def choose_data_file_location():
+    rpm_install = True
 
-
-def choose_data_files():
-    rpminstall = True
-    userinstall = False
-     
     if 'bdist_rpm' in sys.argv:
-        rpminstall = True
+        rpm_install = True
 
-    elif 'install' in sys.argv:
-        for a in sys.argv:
-            if a.lower().startswith('--home'):
-                rpminstall = False
-                userinstall = True
-                
-    if rpminstall:
-        return rpm_data_files
-    elif userinstall:
-        return home_data_files
+    elif '--user' in sys.argv:
+        rpm_install = False
+
+    if rpm_install:
+        return '/etc/vc3'
     else:
         # Something probably went wrong, so punt
-        return rpm_data_files
+        return 'etc'
        
 # ===========================================================
 
 # setup for distutils
-print(choose_data_files())
 print(scripts)
 setup(
     name="vc3-client",
@@ -63,6 +50,6 @@ setup(
     url='http://virtualclusters.org/',
     packages=['vc3client'],
     scripts=scripts,
-    data_files=['etc/vc3-client.conf'],
+    data_files=[(choose_data_file_location(), etc_files)],
     install_requires=['requests', 'pyopenssl', 'cherrypy', 'pyyaml']
 )
