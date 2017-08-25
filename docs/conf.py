f4b4672aed41ea37f0174885a0b9711e316e47e8
AutoPyFactory service account <autopyfactory@apf.virtualclusters.org>
2017-08-25 14:47:18 +0000

    Fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -183,5 +183,6 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 import sys
 import os
 import mock
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
