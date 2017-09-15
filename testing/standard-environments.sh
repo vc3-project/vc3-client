6e8e4caba139308df5a81a45c7f045615fadb9da
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2017-09-15 09:37:25 -0500

    cleaned up what looks like a wonky merge or bad copy/paste, also changed default condor

--- testing/standard-environments.sh
+++ testing/standard-environments.sh
@@ -3,40 +3,7 @@ CLIENT=vc3-client
 CONFIG=/etc/vc3/vc3-client.conf
 #CONFIG=~/git/vc3-client/etc/vc3-client.conf
 #CONFIG=~/vc3/etc/vc3-client.conf
-CONDOR_COLLECTOR=condor.virtualclusters.org
-
-# DEFAULT values
-DEBUG=0
-DEBUGS=""
-
-#######################################################
-
-# read the options
-ARGS=`getopt -o d -l debug -- "$@"`  
-eval set -- "$ARGS"
-
-# extract options and their arguments into variables.
-while true ; do
-    case "$1" in
-        -d|--debug) DEBUG=1 ; shift ;;
-        --) shift ; break ;;
-        *) echo "Internal error!" ; exit 1 ;;
-    esac
-done
-
-# set variables accordingly...
-if [ "$DEBUG" = "1" ] ; then
-   DEBUGS=" -d "
-else
-   DEBUGS=""
-fi
-
-#!/bin/bash -xe
-CLIENT=vc3-client
-CONFIG=/etc/vc3/vc3-client.conf
-#CONFIG=~/git/vc3-client/etc/vc3-client.conf
-#CONFIG=~/vc3/etc/vc3-client.conf
-CONDOR_COLLECTOR=condor.virtualclusters.org
+CONDOR_COLLECTOR=condor-dev.virtualclusters.org
 
 # DEFAULT values
 DEBUG=0
