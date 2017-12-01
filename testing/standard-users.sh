9475a052d8498ba05ce3210e2ba0116e0ce527ae
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2017-12-01 09:52:32 -0600

    website dev was getting confused with double users from the script. I just made a fake user and also added the UUID

--- testing/standard-users.sh
+++ testing/standard-users.sh
@@ -2,20 +2,22 @@
 
 source ./standard-common-config.sh
 
-# Users
-RUN_CHECK_CLIENT user-create --firstname Lincoln --lastname Bryant --email lincolnb@uchicago.edu --institution UChicago lincolnb
+## Users
+RUN_CHECK_CLIENT user-create --firstname Charlie --lastname Root --email root@localhost.tld --institution BellLabs --identity_id="356E5D96-B058-41F2-AD06-B59FC5BE9931" charlie
 
-RUN_CHECK_CLIENT user-create --firstname Benedikt --lastname Riedel --email briedel@uchicago.edu --institution UChicago briedel
-
-RUN_CHECK_CLIENT user-create --firstname Judith --lastname Stephen --email jlstephen@uchicago.edu --institution UChicago jlstephen
-
-RUN_CHECK_CLIENT user-create --firstname Suchandra --lastname Thapa --email ssthapa@uchicago.edu --institution UChicago ssthapa
-
-RUN_CHECK_CLIENT user-create --firstname Jose --lastname Caballero --email caballero@bnl.gov --institution BrookhavenNationalLab caballero
-
-RUN_CHECK_CLIENT user-create --firstname Jeremy --lastname Van --email jeremyvan@uchicago --institution UChicago jeremyvan
-
-RUN_CHECK_CLIENT user-create --firstname Ben --lastname Tovar --email btovar@nd.edu --institution NotreDame btovar
-
-RUN_CHECK_CLIENT user-create --firstname Rob --lastname Gardner --email rwg@uchicago.edu --institution UChicago rwg
-
+#RUN_CHECK_CLIENT user-create --firstname Lincoln --lastname Bryant --email lincolnb@uchicago.edu --institution UChicago lincolnb
+#
+#RUN_CHECK_CLIENT user-create --firstname Benedikt --lastname Riedel --email briedel@uchicago.edu --institution UChicago briedel
+#
+#RUN_CHECK_CLIENT user-create --firstname Judith --lastname Stephen --email jlstephen@uchicago.edu --institution UChicago jlstephen
+#
+#RUN_CHECK_CLIENT user-create --firstname Suchandra --lastname Thapa --email ssthapa@uchicago.edu --institution UChicago ssthapa
+#
+#RUN_CHECK_CLIENT user-create --firstname Jose --lastname Caballero --email caballero@bnl.gov --institution BrookhavenNationalLab caballero
+#
+#RUN_CHECK_CLIENT user-create --firstname Jeremy --lastname Van --email jeremyvan@uchicago --institution UChicago jeremyvan
+#
+#RUN_CHECK_CLIENT user-create --firstname Ben --lastname Tovar --email btovar@nd.edu --institution NotreDame btovar
+#
+#RUN_CHECK_CLIENT user-create --firstname Rob --lastname Gardner --email rwg@uchicago.edu --institution UChicago rwg
+#
