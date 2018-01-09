5307fe2b9fabf7ca3694945c34e72b701a92ba59
Lincoln Bryant <lincolnb@uchicago.edu>
2018-01-09 15:09:37 -0600

    commenting out briedel for now until i get a correct user for him

--- testing/standard-allocations.sh
+++ testing/standard-allocations.sh
@@ -5,5 +5,4 @@ source ./standard-common-config.sh
 # Create allocation
 RUN_CHECK_CLIENT allocation-create --owner lincolnb --resource uchicago-midway --accountname lincolnb lincolnb.uchicago-midway
 RUN_CHECK_CLIENT allocation-create --owner lincolnb --resource uchicago-coreos --accountname lincolnb lincolnb.uchicago-coreos
-RUN_CHECK_CLIENT allocation-create --owner lincolnb --resource nersc-cori      --accountname briedel  briedel.nersc-cori
-
+#RUN_CHECK_CLIENT allocation-create --owner lincolnb --resource nersc-cori      --accountname briedel  briedel.nersc-cori
