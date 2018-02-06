746b5adb60c69a4f407c6fd6397d44c1e26c01ed
Lincoln Bryant <lincolnb@uchicago.edu>
2018-02-06 16:03:50 -0600

    script now more orl ess matches what the website does

--- testing/standard-clusters.sh
+++ testing/standard-clusters.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source ./standard-common-config.sh
 
-# Only one nodeset per cluster for now. Just workers..
-RUN_CHECK_CLIENT nodeset-create --owner lincolnb --node_number 10 --app_type htcondor --app_role worker-nodes htcondor-workers-1
+# Node set
+#Nodeset( name=lincolnb-htcondor-10-workers state=new owner=lincolnb node_number=10 app_type=htcondor app_role=worker-nodes cores=1 memory_mb=None storage_mb=None app_host=None app_port=None app_sectoken=None environment=None description=None displayname=htcondor-10-workers url=None docurl=None )
+RUN_CHECK_CLIENT nodeset-create --owner lincolnb --node_number 10 --app_type htcondor --app_role worker-nodes --displayname="htcondor-10-workers-nodeset" htcondor-10-workers-nodeset
 
-# virtual cluster holder
-RUN_CHECK_CLIENT cluster-create --owner lincolnb htcondor-10workers
+# Cluster containing that nodeset
+# Cluster( name=lincolnb-htcondor-10-workers state=new owner=lincolnb nodesets=[u'lincolnb-htcondor-10-workers'] description=10 HTCondor workers  displayname=htcondor-10-workers url=None docurl=None )
+RUN_CHECK_CLIENT cluster-create --owner lincolnb --description "HTCondor with 10 Workers" --displayname="htcondor-10-workers" htcondor-10-workers
 
-# Add nodeset to cluster
-RUN_CHECK_CLIENT cluster-addnodeset htcondor-10workers htcondor-workers-1
+# Add the nodeset to the cluster
+RUN_CHECK_CLIENT cluster-addnodeset htcondor-10-workers htcondor-10-workers-nodeset
