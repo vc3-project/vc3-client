9e48980232404c386bdc15685ef22945f30609ad
Benjamin Tovar <btovar@nd.edu>
2018-02-26 08:39:42 -0500

    create vc3-team project

--- testing/standard-projects.sh
+++ testing/standard-projects.sh
@@ -3,8 +3,13 @@
 source ./standard-common-config.sh
 
 RUN_CHECK_CLIENT project-create --owner lincolnb --members lincolnb lincolnb
-RUN_CHECK_CLIENT project-create --owner btovar --members btovar btovar
+RUN_CHECK_CLIENT project-create --owner jvan --members jvan jvan
 RUN_CHECK_CLIENT project-create --owner rwg --members rwg rwg
 RUN_CHECK_CLIENT project-create --owner khurtado --members khurtado khurtado
 RUN_CHECK_CLIENT project-create --owner sthapa --members sthapa sthapa
-RUN_CHECK_CLIENT project-create --owner jvan --members jvan jvan
+RUN_CHECK_CLIENT project-create --owner btovar --members btovar btovar
+
+RUN_CHECK_CLIENT project-create --owner btovar --members lincolnb,jvan,rwg,khurtado,sthapa,btovar vc3-team
+
+RUN_CHECK_CLIENT project-addallocation vc3-team btovar.ndccl
+
