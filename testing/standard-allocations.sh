71a07e3b07463500002857b0e0fe5e285510f1d2
Benjamin Tovar <btovar@nd.edu>
2018-02-27 12:33:31 -0500

    validate allocations in standard-allocations.sh

--- testing/standard-allocations.sh
+++ testing/standard-allocations.sh
@@ -6,6 +6,17 @@ source ./standard-common-config.sh
 #Allocation( name=lincolnb.uchicago-midway state=authconfigured owner=lincolnb resource=uchicago-midway type=unlimited accountname=lincolnb quantity=None units=None sectype=ssh-rsa description=Lincolns midway account displayname=lincolnb-midway url=None docurl=None )
 RUN_CHECK_CLIENT allocation-create --owner lincolnb --resource uchicago-midway --accountname lincolnb --description "My Midway account at RCC" --displayname lincolnb-midway lincolnb.uchicago-midway 
 
+RUN_CHECK_CLIENT allocation-validate --allocationname lincolnb.uchicago-midway 
+
+
+
 RUN_CHECK_CLIENT allocation-create --owner btovar --resource ndccl --accountname btovar --description "ND CCL" --displayname btovar-ndccl btovar.ndccl
 
+RUN_CHECK_CLIENT allocation-validate --allocationname btovar.ndccl
+
+
+
 RUN_CHECK_CLIENT allocation-create --owner khurtado --resource osg-connect --accountname khurtado --description "OSG Connect" --displayname khurtado-osgconnect khurtado.osg-connect
+
+RUN_CHECK_CLIENT allocation-validate --allocationname khurtado.osg-connect
+
