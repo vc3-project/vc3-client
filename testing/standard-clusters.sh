122fe5fab2e2c2090cc575e916ed7f9645d263df
Benjamin Tovar <btovar@nd.edu>
2018-09-28 09:47:00 -0400

    fix no --public for nodesets

--- testing/standard-clusters.sh
+++ testing/standard-clusters.sh
@@ -2,26 +2,23 @@
 source ./standard-common-config.sh
 
 
-RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type htcondor --app_role worker-nodes --displayname="htcondor" btovar-htcondor-nodeset --public
+RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type htcondor --app_role worker-nodes --displayname="htcondor" btovar-htcondor-nodeset
 RUN_CHECK_CLIENT cluster-create --owner btovar --description "htcondor" --displayname="htcondor" btovar-htcondor-cluster --public
 
 RUN_CHECK_CLIENT cluster-addnodeset btovar-htcondor-cluster btovar-htcondor-nodeset
 
 
-RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type spark --app_role worker-nodes --displayname="spark" btovar-spark-nodeset --public
+RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type spark --app_role worker-nodes --displayname="spark" btovar-spark-nodeset
 RUN_CHECK_CLIENT cluster-create --owner btovar --description "spark" --displayname="spark" btovar-spark-cluster --public
-
 RUN_CHECK_CLIENT cluster-addnodeset btovar-spark-cluster btovar-spark-nodeset
 
 
-RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type workqueue --app_role worker-nodes --displayname="workqueue" btovar-workqueue-nodeset --public
+RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type workqueue --app_role worker-nodes --displayname="workqueue" btovar-workqueue-nodeset
 RUN_CHECK_CLIENT cluster-create --owner btovar --description "workqueue" --displayname="workqueue" btovar-workqueue-cluster --public
-
 RUN_CHECK_CLIENT cluster-addnodeset btovar-workqueue-cluster btovar-workqueue-nodeset
 
 
-RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type jupyter --app_role worker-nodes --displayname="jupyter" btovar-jupyter-nodeset --public
+RUN_CHECK_CLIENT nodeset-create --owner btovar --node_number 0 --app_type jupyter --app_role worker-nodes --displayname="jupyter" btovar-jupyter-nodeset
 RUN_CHECK_CLIENT cluster-create --owner btovar --description "jupyter" --displayname="jupyter" btovar-jupyter-cluster --public
-
 RUN_CHECK_CLIENT cluster-addnodeset btovar-jupyter-cluster btovar-jupyter-nodeset
 
